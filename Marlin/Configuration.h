f2edc4635121a9a9135f580a904f26ec4c981b3e
Peter Biro <github@vlx.dk>
2021-04-27 22:00:46 +0200

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -131,7 +131,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "vLX winice SKR"
+#define CUSTOM_MACHINE_NAME "vLX winice v1.1 SKR"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
